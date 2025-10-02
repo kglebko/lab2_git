c09898248e29dd13db714197d4a2fd9e5ae4bffe
divanochka <petkundiana.itpaa@gmil.com>
2025-10-02 19:05:12 +0300

    Загружаю лабораторную

--- app/src/main/java/com/example/lab_git/MainActivity.java
+++ app/src/main/java/com/example/lab_git/MainActivity.java
@@ -4,21 +4,87 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private EditText inputHours, inputMinutes;
+    private Button btnCalculate;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        datePicker = findViewById(R.id.datePicker);
+        timePicker = findViewById(R.id.timePicker);
+        inputHours = findViewById(R.id.inputHours);
+        inputMinutes = findViewById(R.id.inputMinutes);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvResult = findViewById(R.id.tvResult);
+
+        timePicker.setIs24HourView(true);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth();
+                int day = datePicker.getDayOfMonth();
+                int hour = timePicker.getHour();
+                int minute = timePicker.getMinute();
+
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, day, hour, minute);
+                Date date = calendar.getTime();
+
+
+                int addHours = 0, addMinutes = 0;
+                try {
+                    addHours = Integer.parseInt(inputHours.getText().toString());
+                }
+                catch (Exception ignored) {}
+
+                try {
+                    addMinutes = Integer.parseInt(inputMinutes.getText().toString());
+                }
+                catch (Exception ignored) {}
+
+
+                calendar.add(Calendar.HOUR_OF_DAY, addHours);
+                calendar.add(Calendar.MINUTE, addMinutes);
+                Date newDate = calendar.getTime();
+
+                // форматированный вывод DateTimeFormatter
+                LocalDateTime localDateTime = newDate.toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime();
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+                String result = localDateTime.format(formatter);
+
+
+                tvResult.setText("Результат: " + result);
+            }
         });
     }
 }
